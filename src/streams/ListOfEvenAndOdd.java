6859c79c1f9c773a7a1f4f18d4d2ed4bc446d183
Alexandre Gaspar <alexandretiao16@gmail.com>
2024-05-19 22:18:22 +0100

    feat(update challenge 16): update using a static function to reuse it to generate both list
    Signed-off-by: Alexandre Gaspar <alexandretiao16@gmail.com>

--- src/streams/ListOfEvenAndOdd.java
+++ src/streams/ListOfEvenAndOdd.java
@@ -8,10 +8,14 @@ public class ListOfEvenAndOdd {
     public static void main(String[] args) {
         List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
 
-        List<Integer> oddNumbers = numbers.stream().distinct().filter(n -> n % 2 != 0).toList();
-        List<Integer> evenNumbers = numbers.stream().distinct().filter(n -> n % 2 == 0).toList();
+        List<Integer> oddNumbers = filterList(numbers, n -> n % 2 != 0);
+        List<Integer> evenNumbers = filterList(numbers, n -> n % 2 == 0);
 
         System.out.println("Even numbers: " + evenNumbers);
         System.out.println("Odd numbers: " + oddNumbers);
+    }
+
+    static List<Integer> filterList(List<Integer> list, Predicate<Integer> p) {
+        return list.stream().distinct().filter(p).toList();
     }
 }
