4cfef31bec98c88719c8a0c6b9596e9b85e5a767
Alexandre Gaspar <alexandretiao16@gmail.com>
2024-05-19 22:02:58 +0100

    update challenge 10 change file name
    Signed-off-by: Alexandre Gaspar <alexandretiao16@gmail.com>

--- src/streams/CheckIfAnyNumbersInsideTheListIsGreaterThanTen.java
+++ src/streams/CheckIfAnyNumbersInsideTheListIsGreaterThanTen.java
@@ -3,7 +3,7 @@ package streams;
 import java.util.Arrays;
 import java.util.List;
 
-public class CheckIfAnyNumbersOfTheListIsPositive {
+public class CheckIfAnyNumbersInsideTheListIsGreaterThanTen {
     public static void main(String[] args) {
         List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
 
